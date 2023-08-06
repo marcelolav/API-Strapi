01538db33d88078685063bfc3f5e3809bb6f77de
marcelolav <marcelo.lavandeira@gmail.com>
2023-08-05 23:30:21 -0300

    Commit inicial sin configurar casi nada

---
+++ src/api/rubro/controllers/rubro.ts
@@ -0,0 +1,7 @@
+/**
+ * rubro controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::rubro.rubro');
---
+++ src/api/rubro/routes/rubro.ts
@@ -0,0 +1,7 @@
+/**
+ * rubro router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::rubro.rubro');
